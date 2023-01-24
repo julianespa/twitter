2b283023d9ab16878aeec05775e63aac269944fd
julianespa <julianespa12@gmail.com>
2023-01-24 15:19:11 +1000

    production on render.com

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 module.exports = {
     debugMode: false,
     api: {
-        host: "https://mini-twitter.onrender.com/api" //"http://api.localhost:80"
+        host: "https://mini-twitter.onrender.com/api" //"http://api.localhost:80" "http://localhost:80/api"
     },
     tweets: {
         maxTweetSize: 140
     }
 }
-
-//https://minitwitterapi.reactiveprogramming.io
